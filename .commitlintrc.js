32358f9246247317b748a791e1b359da3264f3e7
Diachao <3430737927@qq.com>
2023-01-20 15:18:19 +0800

    666

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,11 +1,3 @@
-/*
- * @Author: error: git config user.name && git config user.email & please set dead value or install git
- * @Date: 2023-01-16 20:48:34
- * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2023-01-16 20:48:43
- * @FilePath: \BitBounceFE-UI\.commitlintrc.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 // .commitlintrc.js
 module.exports = {
   extends: ["@commitlint/config-conventional"]
