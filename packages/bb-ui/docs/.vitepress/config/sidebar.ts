68248950adbd0c580cfe04a2bf5b311c821edb62
Xia Yuang <xiayuang@foxmail.com>
2023-02-01 16:05:49 +0800

    fix(components): fix progress merge conflicts
    packages/bb-ui/docs/.vitepress/config/sidebar.ts, packages/bb-ui/docs/.vitepress/theme/index.ts

--- packages/bb-ui/docs/.vitepress/config/sidebar.ts
+++ packages/bb-ui/docs/.vitepress/config/sidebar.ts
@@ -1,37 +1,86 @@
 /*
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2023-01-16 22:12:52
- * @LastEditors: XinyueShu xshuac@connect.hk.ust
- * @LastEditTime: 2023-01-23 17:19:19
+ * @LastEditors: Xia Yuang xiayuang@foxmail.com
+ * @LastEditTime: 2023-02-01 15:53:45
  * @FilePath: \BitBounceFE-UI\packages\bb-ui\docs\.vitepress\config\sidebar.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: doc sidebar
  */
+
 export default {
-  "/": [
+  '/': [
     {
-      text: "快速开始",
-      link: "/",
+      text: '快速开始',
+      link: '/',
       items: [
         {
-          text: "简介",
-          link: "/introduce",
-        },
-      ],
+          text: '简介',
+          link: '/introduce'
+        }
+      ]
     },
     {
-      text: "数据展示",
+      text: '通用',
+      items: []
+    },
+    {
+      text: '导航',
       items: [
         {
-          text: "Card 卡片",
-          link: "/components/card/",
-          status: "100%",
+          text: 'Menu 菜单',
+          link: '/components/menu/',
+          status: '100%'
+        }
+      ]
+    },
+    {
+      text: '反馈',
+      items: [
+        {
+          text: 'Dialog 对话框',
+          link: '/components/dialog/',
+          status: '100%'
         },
         {
-          text: "Progress 进度条",
-          link: "components/progress/",
-          status:"100%",
-        },
-      ],
+          text: 'MessageBox 弹窗',
+          link: '/components/message-box/',
+          status: '100%'
+        }
+      ]
     },
-  ],
+    {
+      text: '数据录入',
+      items: [
+        {
+          text: 'Input 输入框',
+          link: '/components/input/',
+          status: '100%'
+        },
+        {
+          text: 'upload 上传文件',
+          link: '/components/upload/',
+          status: '100%'
+        }
+      ]
+    },
+    {
+      text: '数据展示',
+      items: [
+        {
+          text: 'Card 卡片',
+          link: '/components/card/',
+          status: '100%'
+        },
+        {
+          text: 'Progress 进度条',
+          link: 'components/progress/',
+          status: '100%'
+        }
+      ]
+    },
+    {
+      text: '布局',
+      items: []
+    }
+  ]
 };
--- packages/bb-ui/docs/.vitepress/theme/index.ts
+++ packages/bb-ui/docs/.vitepress/theme/index.ts
@@ -1,25 +1,42 @@
 /*
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2023-01-16 22:13:21
- * @LastEditors: XinyueShu xshuac@connect.hk.ust
- * @LastEditTime: 2023-01-23 17:16:03
+ * @LastEditors: Xia Yuang xiayuang@foxmail.com
+ * @LastEditTime: 2023-02-01 15:49:55
  * @FilePath: \BitBounceFE-UI\packages\bb-ui\docs\.vitepress\theme\index.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 组件注册
  */
-import Theme from "vitepress/theme";
-import "./styles/index.scss";
-import "vitepress-theme-demoblock/theme/styles/index.css";
-import { registerComponents } from "./register-components.js";
+import Theme from 'vitepress/theme';
+import './styles/index.scss';
+import 'vitepress-theme-demoblock/theme/styles/index.css';
+import { registerComponents } from './register-components.js';
 // 引入组件 注册
-import CardInstall from "../../../ui/card/index";
-import ProgreInstall from "../../../ui/progress-component/index";
+import CardInstall from '../../../ui/card/index';
+
+import DialogInstall from '../../../ui/dialog/index';
+import MenuInstall from '../../../ui/menu';
+import MessageBoxInstall from '../../../ui/message-box';
+import ProgreInstall from '../../../ui/progress-component/index';
+
+import UploadInstall from '../../../ui/upload/index';
+import SelectInstall from '../../../ui/select/index';
+import InputInstall from '../../../ui/input/index';
 // 引入组件 注册
+
 export default {
   ...Theme,
   enhanceApp({ app }) {
     //  注册组件
     app.use(CardInstall);
     app.use(ProgreInstall);
+    app.use(DialogInstall);
+    app.use(MenuInstall);
+    app.use(MessageBoxInstall);
+
+    app.use(UploadInstall);
+
+    app.use(SelectInstall);
+    app.use(InputInstall);
     registerComponents(app);
-  },
+  }
 };
