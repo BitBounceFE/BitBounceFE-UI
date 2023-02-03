44bfd0fe543548c06a81df61b84b4984ffecb0c9
xuepeng184 <1831919639@qq.com>
2023-02-03 17:48:56 +0800

    refactor: extract common css variables

--- packages/bb-ui/ui/progress/src/progress.tsx
+++ packages/bb-ui/ui/progress/src/progress.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2023-01-16 21:13:04
- * @LastEditors: XinyueShu xshuac@connect.hk.ust
- * @LastEditTime: 2023-01-29 21:53:24
+ * @LastEditors: xuepeng184 1831919639@qq.com
+ * @LastEditTime: 2023-02-03 16:54:07
  * @FilePath: \BitBounceFE-UI\packages\bb-ui\ui\card\src\card.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
