c91a8e38b8353bbebc0bdc4a3c43dd40beb8e798
syk <syk@qq.com>
2023-02-14 00:27:44 +0800

    test(components): basic test for select

--- packages/bb-ui/ui/select/test/select.test.ts
+++ packages/bb-ui/ui/select/test/select.test.ts
@@ -2,7 +2,7 @@
  * @Author: syk syk@qq.com
  * @Date: 2023-02-11 11:19:04
  * @LastEditors: syk syk@qq.com
- * @LastEditTime: 2023-02-12 22:08:18
+ * @LastEditTime: 2023-02-14 00:26:01
  * @FilePath: \组件库\BitBounceFE-UI\packages\bb-ui\ui\select\test\select.test.ts
  * @Description: unit test for select
  * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
@@ -17,8 +17,29 @@ const boxclass = ns.b();
 describe('BSelect.vue', () => {
   // select是否打开了
   test('create', () => {
-    const wrapper = mount(Select);
+    const wrapper = mount(Select, {
+      props: {
+        options: [
+          {
+            name: '第一个数字值',
+            value: '1234444'
+          },
+          {
+            name: '第二个数字值',
+            value: '453336'
+          }
+        ]
+      }
+    });
     expect(wrapper.classes()).toContain(boxclass);
     wrapper.unmount();
+  });
+  // test props
+  test('props', () => {
+    const wrapper = mount(Select, {
+      size: 'mid'
+    });
+    console.log(wrapper.html());
+    expect(wrapper.classes()).toContain('bbui-select-mid');
   });
 });
