012437581cd83f9f1e222e69d4e6c4e5a12876f0
-yayabb <2286834433@qq.com>
2023-02-15 12:49:25 +0800

    test(components): 添加message-box测试用例

---
+++ packages/bb-ui/ui/message-box/test/message-box.test.ts
@@ -0,0 +1,56 @@
+/* eslint-disable prettier/prettier */
+import { mount } from '@vue/test-utils';
+import { nextTick, h} from 'vue';
+import { expect, test, describe} from 'vitest';
+import MessageBox from '../src/message-box';
+describe('BMessageBox.vue', async () => {
+  // 测试mask
+  test('render test', async () => {
+    // 挂载组件，得到这个包裹器
+    const wrapper = mount(MessageBox);
+    await nextTick();
+    // 默认为false
+    expect(wrapper.find('.bbui-message-box__mask').isVisible()).toBe(false);
+    wrapper.unmount();
+  });
+  // props test
+  test('common props test', async () => {
+    const wrapper = mount(MessageBox, {
+      props: {
+        title: 'header title',
+      }
+    });
+    await nextTick();
+    expect(wrapper.html()).toContain('header title');
+    wrapper.unmount();
+  });
+
+  // delay test
+  test('delay props test', async (done) => {
+    const wrapper = mount(MessageBox, {
+      props: {
+        openDelay: 100,
+        closeDelay: 100
+      }
+    });
+    await wrapper.setProps({ vModel: true });
+    expect(wrapper.find('.bbui-message-box__mask').isVisible()).toBe(false);
+    wrapper.unmount();
+  });
+
+  // slots test
+  test('emit test', async () => {
+    const wrapper = mount(MessageBox, {
+      slots: {
+        header: h('h1', {}, 'header slot'),
+        default: 'Default',
+        footer: h('div', {}, 'footer slot')
+      }
+    });
+    await nextTick();
+    expect(wrapper.html()).toContain('<h1>header slot</h1>');
+    expect(wrapper.html()).toContain('Default');
+    expect(wrapper.html()).toContain('<div>footer slot</div>');
+    wrapper.unmount();
+  });
+});
