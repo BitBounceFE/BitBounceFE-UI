6b2519341150af607c730dc7aae02fc1d9e6912e
-yayabb <2286834433@qq.com>
2023-02-20 20:20:43 +0800

    docs(other): 修改名称

--- packages/bb-ui/docs/.vitepress/config/index.ts
+++ packages/bb-ui/docs/.vitepress/config/index.ts
@@ -2,7 +2,7 @@
  * @Author: -yayabb 2286834433@qq.com
  * @Date: 2023-02-20 13:31:32
  * @LastEditors: -yayabb 2286834433@qq.com
- * @LastEditTime: 2023-02-20 19:06:38
+ * @LastEditTime: 2023-02-20 20:07:27
  * @FilePath: \BitBounceFE-UI\packages\bb-ui\docs\.vitepress\config\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,8 +13,8 @@ import sidebar from './sidebar';
 export default {
   // base: '/BitBounceFE-UI/',
   lang: 'zh',
-  title: 'bb-ui',
-  description: 'bb-ui 组件库',
+  title: 'bitbouncefe-ui',
+  description: 'bitbouncefe-ui 组件库',
   lastUpdated: true,
   // TODO 这里需要改
   head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }]],
