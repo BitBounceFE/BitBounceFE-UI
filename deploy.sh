35285e7f030e30b4730c7fa011b206362366c25f
Xia Yuang <xiayuang@foxmail.com>
2023-02-20 17:57:40 +0800

    docs(utils): fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # navigate into the build output directory
-cd packages/bb-ui/docs/.vitepress
+cd packages/bb-ui/docs/.vitepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
